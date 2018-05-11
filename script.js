9c78c6a8ce904011ab7890c5a8b9004be8ba9676
tiffanydemps <35701572+tiffanydemps@users.noreply.github.com>
2018-05-11 00:16:28 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+        //add and remove at the same time
+
+      //   $("#show").addClass( "findouthow" ).removeClass( "checkagainlater" )
+      // });
+
+
+       $(".box").click(function(){
+            $(".box").hide();
+        });
+
+
+
+$("#replacetext").hover(function(){
+$("#replace").html("<h5>Be a happy worker!</h5>");
+});
+
+
+$("#thanks").hover(function(){
+  $(".bottom").hide();
+});
+
+<script>
+// Get the video
+var video = document.getElementById("code");
+
+// Get the button
+var btn = document.getElementById("playbutton");
+
+// Pause and play the video, and change the button text
+function myFunction() {
+    if (video.paused) {
+        video.play();
+        btn.innerHTML = "Pause";
+    } else {
+        video.pause();
+        btn.innerHTML = "Play";
+    }
+}
+</script>
+
+});
